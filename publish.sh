fdc8dae8818859d079c9e6886a4e12aacfb255a7
Jean BERNARD <jean.m.bernard@gmail.com>
2025-09-17 09:49:16 +0200

    publish

--- publish.sh
+++ publish.sh
@@ -4,12 +4,20 @@ sed -i "s|<version_tag>|<version_tag>\n- $1 : $2|g" README.md
 #git add *.php *.js *.md *.png *.jpg *.sh *.css
 git add *.php
 git add *.jpg
+git add *.png
 git add *.sh
 git add *.md
 git add *.sql
 git add *.txt
 git add *.htaccess
 git add *.py
+git add *.cfg
+git add *.env
+git add *.ini
+git add *.json
+git add *.log
+git add *.yml
+git add *.yaml
 git commit -m "$2"
 git tag -a $1 -m "$2"
 git push
